82085c0f906e21ed6d2d4a4f93ced90ccebab589
Mariano Gonzalez <mariano.gonzalez@uptake.com>
2019-03-14 20:26:24 -0500

    Misc fixes

--- vendor/github.com/eschizoid/flixctl/models/sessions.go
+++ vendor/github.com/eschizoid/flixctl/models/sessions.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 func (db *DB) SaveLastActiveSession(time time.Time) error {
-	err := db.Set(plexSessionsBucketName, "last_activity", time)
+	err := db.Set(plexSessionsBucketName, "last_activity", time.String())
 	return err
 }
 
-func (db *DB) GetLastActiveSession() (lastActiveTime time.Time, err error) {
-	err = db.Get(plexSessionsBucketName, "last_activity", &lastActiveTime)
+func (db *DB) GetLastActiveSession() (time.Time, error) {
+	var lastActiveTime time.Time
+	err := db.Get(plexSessionsBucketName, "last_activity", &lastActiveTime)
 	return lastActiveTime, err
 }
